48b1553efce19f7d3581e5333ae9fb2b407b6f6a
smalaca <ab13.krakow@gmail.com>
2022-02-05 14:44:17 +0100

    REFACT - remove duplicated logic in TeamController by using Team.asDto method

--- src/main/java/com/smalaca/taskamanager/api/rest/TeamController.java
+++ src/main/java/com/smalaca/taskamanager/api/rest/TeamController.java
@@ -45,20 +45,7 @@ public class TeamController {
     @GetMapping
     public ResponseEntity<List<TeamDto>> findAll() {
         List<TeamDto> teams = StreamSupport.stream(teamRepository.findAll().spliterator(), false)
-                .map(team -> {
-                    TeamDto dto = new TeamDto();
-                    dto.setId(team.getId());
-                    dto.setName(team.getName());
-
-                    if (team.hasCodename()) {
-                        dto.setCodenameShort(team.getCodename().getShortName());
-                        dto.setCodenameFull(team.getCodename().getFullName());
-                    }
-
-                    dto.setDescription(team.getDescription());
-
-                    return dto;
-                })
+                .map(Team::asDto)
                 .collect(toList());
 
         return new ResponseEntity<>(teams, HttpStatus.OK);
@@ -119,17 +106,7 @@ public class TeamController {
 
         Team updated = teamRepository.save(team);
 
-        TeamDto dto = new TeamDto();
-        dto.setId(updated.getId());
-        dto.setName(updated.getName());
-        if (updated.hasCodename()) {
-            dto.setCodenameShort(updated.getCodename().getShortName());
-            dto.setCodenameFull(updated.getCodename().getFullName());
-        }
-
-        dto.setDescription(updated.getDescription());
-
-        return new ResponseEntity<>(dto, HttpStatus.OK);
+        return new ResponseEntity<>(updated.asDto(), HttpStatus.OK);
     }
 
     @PutMapping("/{id}/members")
