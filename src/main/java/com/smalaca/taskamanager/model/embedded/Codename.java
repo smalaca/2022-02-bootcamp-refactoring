3e60180cb6678b6b92d757b4b06be30224f52d29
smalaca <ab13.krakow@gmail.com>
2022-02-05 14:11:08 +0100

    REFACT - use lombok to remove boilerplate code from Codename

--- src/main/java/com/smalaca/taskamanager/model/embedded/Codename.java
+++ src/main/java/com/smalaca/taskamanager/model/embedded/Codename.java
@@ -1,49 +1,13 @@
 package com.smalaca.taskamanager.model.embedded;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode
 public class Codename {
     private final String shortName;
     private final String fullName;
-
-    public Codename(String codenameShort, String codenameFull) {
-        shortName = codenameShort;
-        fullName = codenameFull;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Codename codename = (Codename) o;
-
-        return new EqualsBuilder()
-                .append(shortName, codename.shortName)
-                .append(fullName, codename.fullName)
-                .isEquals();
-    }
-
-    @Override
-    @SuppressWarnings("MagicNumber")
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(shortName)
-                .append(fullName)
-                .toHashCode();
-    }
 }
